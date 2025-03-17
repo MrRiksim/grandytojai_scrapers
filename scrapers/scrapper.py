2916fe3b602b5e9bca1798cebeff079a55c7b6fb
JuozasPetryla <juopet2@ktu.lt>
2025-03-17 18:39:03 +0200

    EPT-22 kilobaitas scrapper created

---
+++ scrapers/scrapper.py
@@ -0,0 +1,10 @@
+from scrapers.api.api import ApiClient
+
+class Scrapper:
+    def __init__(self):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        self.api_client = ApiClient()
+        self.computer_part_endpoint = "computerParts"
+        self.created_count = 0
+        self.updated_count = 0
+        self.total_scrapped_count = 0
