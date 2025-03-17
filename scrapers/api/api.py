8ac6ca557ea9d286ac3faa0b6dd7ea4290c37a7d
JuozasPetryla <juopet2@ktu.lt>
2025-03-17 21:06:45 +0200

    Fix early api error

--- scrapers/api/api.py
+++ scrapers/api/api.py
@@ -15,21 +15,9 @@ class ApiClient:
         }
 
     def post_data(self, endpoint: str, payload: Generic[T]) -> Response:
-        try: 
-            response = post(url=urljoin(BACKEND_BASE_URL_DEV, endpoint), data=json.dumps(payload), headers=self.headers)
-            response.raise_for_status()
-            return response
-        except HTTPError as e:
-            raise HTTPError(f"A HTTP error has occured when performing post request: {e}", response=e.response, request=e.request)
-        except Exception as e:
-            raise Exception(f"An unexpected exception occured: {e}")
+        response = post(url=urljoin(BACKEND_BASE_URL_DEV, endpoint), data=json.dumps(payload), headers=self.headers)
+        return response
         
     def put_data(self, endpoint: str, payload: Generic[T]) -> Response:
-        try: 
-            response = put(url=urljoin(BACKEND_BASE_URL_DEV, endpoint), data=json.dumps(payload), headers=self.headers)
-            response.raise_for_status()
-            return response
-        except HTTPError as e:
-            raise Exception(f"A HTTP error has occured when performing put request: {e}", response=e.response, request=e.request)
-        except Exception as e:
-            raise Exception(f"An unexpected exception occured: {e}")
+        response = put(url=urljoin(BACKEND_BASE_URL_DEV, endpoint), data=json.dumps(payload), headers=self.headers)
+        return response
